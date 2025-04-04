d1425113433b2ab893f0b35a56949260aa4d5014
Ramon Xavier <ramon1234rxm@gmail.com>
2025-04-03 21:03:07 -0300

    add bool example

---
+++ fundamentals/bool.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	// Boolean
+	var isTrue bool = true
+	var isFalse bool = false
+	fmt.Println(isTrue, isFalse)
+
+	// Expressions
+	fmt.Println(10 > 5)
+	fmt.Println(10 < 5)
+	fmt.Println(10 == 5)
+	fmt.Println(10 != 5)
+
+}
